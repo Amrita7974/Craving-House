944987d0ce937962743d93ef302c361d74bc9450
Amrita7974 <amritakumari9343@gmail.com>
2026-07-29 07:45:00 +0530

    feat: Enhance Menu and Restaurant schemas; add new fields for item status, food type, and legal information

--- Server/src/models/restaurant.model.js
+++ Server/src/models/restaurant.model.js
@@ -8,11 +8,11 @@ const RestaurantSchema = mongoose.Schema(
       required: true,
     },
     restaurantName: { type: String, required: true },
-    address: { type: String, required: true },
-    city: { type: String, required: true },
-    state: { type: String, required: true },
-    pinCode: { type: String, required: true },
-    country: { type: String, required: true },
+    address: { type: String, default: "" },
+    city: { type: String, default: "" },
+    state: { type: String, default: "" },
+    pinCode: { type: String, default: "" },
+    country: { type: String, default: "" },
     geoLocation: {
       type: {
         lat: {
@@ -23,11 +23,17 @@ const RestaurantSchema = mongoose.Schema(
         },
       },
     },
-
-    documents: {
+    legal: {
       type: {
         legalName: { type: String, required: true },
         companyType: { type: String, required: true },
+      },
+    },
+
+    documents: {
+      type: {
+       
+        
         gstCertificate: { type: String, required: true },
         fssaiCertificate: { type: String, required: true },
         panCard: { type: String, required: true },
