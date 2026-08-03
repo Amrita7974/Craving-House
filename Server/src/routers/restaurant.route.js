2f6d5e1647f398f977d03dc987a633ee220be415
Amrita7974 <amritakumari9343@gmail.com>
2026-08-03 22:34:42 +0530

    feat: Add restaurant menu management routes; implement add, update, toggle, and delete functionality

--- Server/src/routers/restaurant.route.js
+++ Server/src/routers/restaurant.route.js
@@ -2,12 +2,23 @@ import express from "express";
 import multer from "multer";
 import {
   RestaurantUpdateProfile,
-  RestaurantGetData,
+  RestaurantGetData, 
+  RestaurantUpdateInfo,
+  OpenRestaurant,
+  RestaurantUpdateLegalInfo,
+  RestaurantAddMenuItem,
+  RestaurantMenuItems,
+   RestaurantUpdateMenuItem,
+  RestaurantUpdateMenuItemStatus,
+  RestaurantToggleMenuItemControl,
+  RestaurantDeleteMenuItem,
 } from "../controllers/restaurant.controller.js";
 import { RestaurantAuthProtect } from "../middlewares/auth.middleware.js";
 
 const upload = multer();
 const router = express.Router();
+
+
 
 router.post(
   "/update-profile",
@@ -18,5 +29,59 @@ router.post(
 );
 
 router.get("/get-resturant-data", RestaurantAuthProtect, RestaurantGetData);
+
+
+router.put(
+  "/update-restaurant-info",
+  RestaurantAuthProtect,
+  RestaurantUpdateInfo,
+);
+
+router.patch(
+  "/change-open-status/:openStatus",
+  RestaurantAuthProtect,
+  OpenRestaurant,
+);
+
+router.put(
+  "/update-legal-info",
+  RestaurantAuthProtect,
+  RestaurantUpdateLegalInfo,
+); 
+
+
+router.get("/menu-items", RestaurantAuthProtect, RestaurantMenuItems);
+
+router.post(
+  "/add-menu-item",
+  RestaurantAuthProtect,
+  upload.single("itemImage"),
+  RestaurantAddMenuItem,
+);
+
+router.put(
+  "/menu-item/:itemId",
+  RestaurantAuthProtect,
+  upload.single("itemImage"),
+  RestaurantUpdateMenuItem,
+);
+
+router.patch(
+  "/menu-item/:itemId/status",
+  RestaurantAuthProtect,
+  RestaurantUpdateMenuItemStatus,
+);
+
+router.patch(
+  "/menu-item/:itemId/control",
+  RestaurantAuthProtect,
+  RestaurantToggleMenuItemControl,
+);
+
+router.delete(
+  "/menu-item/:itemId",
+  RestaurantAuthProtect,
+  RestaurantDeleteMenuItem,
+);
 
 export default router;
